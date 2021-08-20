8913a149d2188825090a4f307aa6b9e7f6b79571
Angel Huerta <angeladrianhp@gmail.com>
2021-08-20 14:58:35 -0500

    Login component

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Container from '@material-ui/core/Container';
+import Avatar from '@material-ui/core/Avatar';
+
+import '../../assets/styles/components/Login.scss';
+
+const Login = () => {
+  return (
+    <div className="container-login">
+      <Container fixed>
+        <Avatar>AH</Avatar>
+      </Container>
+    </div>
+  );
+};
+
+export default Login;
